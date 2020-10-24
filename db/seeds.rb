a4f3f07a341808cb8555eb547f15db2cb1baa868
kogirika <kogirika12@gmail.com>
2020-10-24 18:27:41 +0900

    devise関連のviewを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  [
+    {
+      name: 'hoge',
+      email: 'hoge@example.com',
+      password: 'hoge12'
+    },
+    {
+      name: 'hoge2',
+      email: 'hoge2@example.com',
+      password: 'hoge12'
+    }
+  ]
+)
+# User.all.each do |user|
+#   user.selections.create!(
+#       name: "A社",
+#       phase_id: "0",
+#       entry_day: "2020-10-10",
+#       refusal_reason: "",
+#       memo: "hogehoge",
+#       live: true
+#   )
+# end
