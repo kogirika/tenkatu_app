65d0c0060fa54fa3648967ccfd3fa5f1eb2959da
kogirika <kogirika12@gmail.com>
2020-10-26 00:19:43 +0900

    viewを簡易設定して実登録と表示を確認

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -6,7 +6,8 @@ class SelectionsController < ApplicationController
   end
 
   def create
-    @selection = Selection.new
+    @selection = Selection.new(selection_params)
+    live_flg = true
     if @selection.save
       redirect_to root_path
     else
@@ -15,4 +16,9 @@ class SelectionsController < ApplicationController
     end
   end
 
+  private
+
+  def selection_params
+    params.require(:selection).permit(:company_name, :position, :entry_day, :phase_id, :live_flg, :agent, :memo).merge(user_id: current_user.id)
+  end
 end
--- app/models/selection.rb
+++ app/models/selection.rb
@@ -3,7 +3,10 @@ class Selection < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :phase
 
-# バリデーション
+  #初期値設定
+  attribute :live_flg, :boolean, default: true
+
+  # バリデーション
 with_options presence: true do
   validates :company_name, :phase_id, :entry_day, :live_flg
 end
