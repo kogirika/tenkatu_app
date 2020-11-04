bbed0a73e8d30d67c8f2bf0b15cdc3795facdcd3
kogirika <kogirika12@gmail.com>
2020-11-04 14:21:37 +0900

    不要バリデーション削除

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,7 +1,6 @@
 class Interview < ApplicationRecord
   # バリデーション
   validates :interview_day, presence: true
-  validate :date_valid?
 
   #アソシエーション
   belongs_to :selection
@@ -12,12 +11,5 @@ class Interview < ApplicationRecord
     data = Interview.where(selection_id: selection_id)
     sort_young(data)
   end
-
-  #不正な時間チェック
-  private
-  def date_valid?
-    !! Date.parse(:entry_day) rescue false
-  end
-
 
 end
