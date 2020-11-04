2f55950931aab5fa820dbdbd0104fa55be54b9aa
kogirika <kogirika12@gmail.com>
2020-11-04 14:12:56 +0900

    Interviewモデル単体テスト

--- spec/factories/interviews.rb
+++ spec/factories/interviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :interview do
-    
+    interview_day         { Faker::Date.in_date_period(month: 2) }
+    charge                { Faker::Internet.username }
+    memo                  { Faker::Lorem.paragraph }
+    association :selection
   end
 end
--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Interview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "selectionの保存" do
+    before do
+      @in = FactoryBot.build(:interview)
+    end
+    context "保存がうまくいくとき" do
+      it "すべての値が正しく入力されていれば保存できること" do
+        expect(@in).to be_valid
+      end
+      it "必須以外が抜けていても保存できること" do
+        @in.charge, @in.memo = nil
+        expect(@in).to be_valid
+      end
+    end
+    context "保存がうまくいかないとき" do
+      it "エントリー日が空だと保存できない" do
+        @in.interview_day = ""
+        @in.valid?
+        expect(@in.errors.full_messages).to include("面接日を入力してください")
+      end
+      it "エントリー日が不正だと保存できない" do
+        @in.interview_day = "ああああ"
+        @in.valid?
+        expect(@in.errors.full_messages).to include("面接日を入力してください")
+      end
+      it "エントリー日が不正だと保存できない" do
+        @in.interview_day = "2020-02-31"
+        @in.valid?
+        expect(@in.errors.full_messages).to include("面接日を入力してください")
+      end
+    end
+  end
 end
