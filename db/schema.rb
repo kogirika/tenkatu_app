46991c234ca544b839ca23be7e7cbca18f314bd7
kogirika <kogirika12@gmail.com>
2020-11-04 11:37:11 +0900

    カラム名の変更concern → anxiety

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_023114) do
     t.string "url"
     t.text "sumally"
     t.text "appeal"
-    t.text "concern"
+    t.text "anxiety"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
